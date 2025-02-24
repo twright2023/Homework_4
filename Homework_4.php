6d18d3985e36bccbff153a1cb740156f42956df0
Tyrell <tjwright2023@gmail.com>
2025-02-24 14:39:45 -0500

    Update Homework_4.php
    Fixed, and added a link to Git

--- Homework_4.php
+++ Homework_4.php
@@ -27,6 +27,10 @@
                             print("<input type='radio' name='bread' value='sd'>Sourdough<br>");
                             print("<input type='radio' name='bread' value='ciabatta'>Ciabatta<br>");
                             print("<input type='radio' name='bread' value='wheat'>Wheat (Rye, they all look the same.)<br>");
+                            //Finally found a place to put this! So, 1 gives you radio buttons of your options. Each option is required, that's what the "required" part is about.
+                            //Each line of code is like this, with the name of the group being bread and each button having its value/name, being a correlation to its visible name.
+                            //This is true for 1-8. The inputs are the buttons, by the way.
+                            //The one link at top and the two scripts at the bottom is for the style!
                         ?>
                         </select>
                     </div>  
@@ -78,17 +82,17 @@
                             print("<input type='radio' name='sav' value='bacon' required>Bacon<br>");  
                             print("<input type='radio' name='sav' value='pickles'>Pickles<br>");
                             print("<input type='radio' name='sav' value='chips'>Potato chips<br>");
-                            print("<input type='radio' name='sav' value='no2'>Nuh Uh<br>");
+                            print("<input type='radio' name='sav' value='no2'>Nuh Uh<br>"); //Only acceptable option. My sister puts Potato chips on her PBJ and ever since I've been looking at her like she's crazy.
                         ?>
                         </select>
                     </div>  
                     <div>
                         <legend>7. How are you cutting it?</legend>
                         <?php
-                            print("<input type='radio' name='cut' value='weird' required>Straight down in half<br>");  
-                            print("<input type='radio' name='cut' value='weirder'>In quarters<br>");
+                            print("<input type='radio' name='cut' value='weird' required>Straight down in half<br>"); //Barbarion, have some class! 
+                            print("<input type='radio' name='cut' value='weirder'>In quarters<br>"); //You only do this if you're making it for your chid
                             print("<input type='radio' name='cut' value='proper'>Diagonally<br>");
-                            print("<input type='radio' name='cut' value='quick'>Whole<br>");
+                            print("<input type='radio' name='cut' value='quick'>Whole<br>");//How I did it ever since I turned 18.
                         ?>
                         </select>
                     </div>  
@@ -105,6 +109,10 @@
                 </fieldset>
             </div>
             <button type="submit" name="button2" id = "button2" class="btn btn-primary">Submit!</button>
+            <div class="topnav">
+                Link to Homework 4 and Commenting Etiqutte in Github:
+                <a class="btn btn-primary" href="https://github.com/twright2023/Homework_4.git" role="button">GitHub</a>
+            </div>
         </form>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
